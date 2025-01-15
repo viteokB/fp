e7183c5168cf664048353a00108c8b50cbd10266
Braidenbikher Victor <viktor.brai@yandex.ru>
2025-01-15 19:01:58 +0500

    ConsoleClient, Program использует Result<T>

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using CommandLine;
 using ConsoleClient.Services;
+using FileSenderRailway;
 
 namespace ConsoleClient;
 
@@ -21,16 +22,36 @@ internal class Program
             DiRegister.RegisterAll(builder, settings);
             var container = builder.Build();
 
-            try
+            var createCloudImageResult = Result.Of(() => container.Resolve<TagCloudImageCreator>())
+                .Then(creator => 
+                    creator.CreateCloudImage(settings.ImageSave, settings.ImageCreate, settings.ReaderSettings));
+
+            if (!createCloudImageResult.IsSuccess)
             {
-                var tagCloudImageCreator = container.Resolve<TagCloudImageCreator>();
-                tagCloudImageCreator.CreateCloudImage(settings.ImageSave, settings.ImageCreate, settings.ReaderSettings);
-                Console.WriteLine("Облако тегов успешно создано.");
+                Console.ForegroundColor = ConsoleColor.Red; // Устанавливаем красный цвет
+                Console.WriteLine();
+                Console.WriteLine("Tag cloud creation error(s)");
+                Console.WriteLine(createCloudImageResult.Error);
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"Произошла ошибка при создании облака тегов: {ex.Message}");
+                Console.ForegroundColor = ConsoleColor.Green; // Устанавливаем зеленый цвет
+                Console.WriteLine();
+                Console.WriteLine("The tag cloud was created successfully");
+                Console.WriteLine($"Image location: {options.PathToSaveImage}");
             }
+
+            Console.ResetColor();
+            //try
+            //{
+            //    var tagCloudImageCreator = container.Resolve<TagCloudImageCreator>();
+            //    tagCloudImageCreator.CreateCloudImage(settings.ImageSave, settings.ImageCreate, settings.ReaderSettings);
+            //    Console.WriteLine("Облако тегов успешно создано.");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Произошла ошибка при создании облака тегов: {ex.Message}");
+            //}
         })
         .WithNotParsed(errors =>
         {
